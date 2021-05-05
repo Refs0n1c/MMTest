f4cc3fd77f4877d46438623a7b00d5f2493d649e
Refs0n1c <daniel.refson2306@gmail.com>
2021-05-05 14:43:32 +0100

    added age calc

---
+++ MMTestAPI/Models/Customer.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DOB { get; set; }
+    public int Age { get {return CalculateAge(DOB);} }
+    
+    
+    
+
+
+
+
+    
+    private int CalculateAge(DateTime DOB)
+    {
+        int age = DateTime.Now.Year - DOB.Year;
+
+        if (DOB > DateTime.Now.AddYears(-age)) 
+        age--;
+
+        return age;
+    }
+    
+    
+}
