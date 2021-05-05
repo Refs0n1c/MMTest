6e081f1df2e0532fe6e6c1f70314c25c99755b8d
Refs0n1c <daniel.refson2306@gmail.com>
2021-05-05 14:36:15 +0100

    initial skeleton

---
+++ MMTestAPI/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MMTestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MMTestController : ControllerBase
+    {
+
+        private readonly ILogger<MMTestController> _logger;
+
+        public MMTestController(ILogger<MMTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers(bool isActive)
+        {
+           return Ok();
+        }
+
+        
+        [HttpPost]
+        public IActionResult RemoveCustomer(bool softDelete)
+        {
+           return Ok();
+        }
+    }
+}
